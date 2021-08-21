5d378ca82a60d936b311e79c4beb4755b1aba35a
JPGallegos1 <43424229+JPGallegos1@users.noreply.github.com>
2021-08-21 08:50:37 -0300

    add styles and layout to the Home page

--- chakra.config.js
+++ chakra.config.js
@@ -1,23 +1,44 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const colors = {
-  quorum: {
-    white: {
-      100: "#EFEFF2",
-    },
-    black: {
-      100: "#23292F",
-      200: "#0D0E0E",
-      300: "#181B21",
-    },
-    gray: {
-      100: "#394251",
-      200: "#B9C1D9",
-    },
-    red: {
-      100: "#EE6855",
+export const theme = extendTheme({
+  styles: {
+    global: (props) => ({
+      "html, body": {
+        background: "quorum.black.200",
+      },
+      a: {
+        textDecoration: "none",
+      },
+    }),
+  },
+  colors: {
+    quorum: {
+      white: {
+        100: "#EFEFF2",
+      },
+      black: {
+        100: "#23292F",
+        200: "#0D0E0E",
+        300: "#181B21",
+      },
+      gray: {
+        100: "#394251",
+        200: "#B9C1D9",
+        300: "#EFEFF2",
+        400: "#8B95B8",
+      },
+      red: {
+        100: "#EE6855",
+      },
     },
   },
-};
-
-export const theme = extendTheme({ colors });
+  textStyles: {
+    h1: {
+      fontSize: ["36px", "42px"],
+      fontFamily: "SF Pro Display",
+      fontWeight: "bold",
+      lineHeight: "50px",
+      letterSpacing: "0.41px",
+    },
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Fonts from "@/components/Fonts";
+
 import { theme } from "../chakra.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Component {...pageProps} />
     </ChakraProvider>
   );
